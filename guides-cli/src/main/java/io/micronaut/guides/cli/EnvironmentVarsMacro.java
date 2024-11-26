b4ecdadcabc869c747cf6df7f4263d1f4a6f3be1
Andrea La Grotteria <andrea.la.grotteria@oracle.com>
2024-11-26 11:25:52 +0100

    Adds EnvironmentVarsMacro. (#23)

---
+++ guides-cli/src/main/java/io/micronaut/guides/cli/EnvironmentVarsMacro.java
@@ -0,0 +1,46 @@
+package io.micronaut.guides.cli;
+
+import io.micronaut.core.io.ResourceLoader;
+import io.micronaut.guides.core.*;
+import io.micronaut.guides.core.asciidoc.AsciidocMacro;
+import io.micronaut.guides.core.asciidoc.Attribute;
+import jakarta.inject.Singleton;
+
+import java.util.Optional;
+
+import static io.micronaut.guides.core.MacroUtils.findMacroLines;
+
+@Singleton
+public class EnvironmentVarsMacro implements MacroSubstitution {
+    private final static String MACRO = "environment-vars";
+    private String html;
+
+    EnvironmentVarsMacro(ResourceLoader resourceLoader, GuidesTemplatesConfiguration guidesTemplatesConfiguration) {
+        this.html = TemplateLoaderUtils.getClasspathTemplate(resourceLoader, guidesTemplatesConfiguration, "consoleTabs.html");
+    }
+
+    @Override
+    public String substitute(String str, Guide guide, GuidesOption option) {
+        for (String line : findMacroLines(str, MACRO)) {
+            Optional<AsciidocMacro> asciidocMacroOptional = AsciidocMacro.of(MACRO, line);
+            if (asciidocMacroOptional.isEmpty()) {
+                continue;
+            }
+
+            AsciidocMacro asciidocMacro = asciidocMacroOptional.get();
+            StringBuilder bashBuilder = new StringBuilder();
+            StringBuilder windowsBuilder = new StringBuilder();
+            StringBuilder powershellBuilder = new StringBuilder();
+
+            for (Attribute attribute : asciidocMacro.attributes()) {
+                String name = attribute.key();
+                String value = attribute.values().get(0);
+                bashBuilder.append("<span class=\"hljs-built_in\">export</span> ").append(name).append("=").append(value).append("\n");
+                windowsBuilder.append("<span class=\"hljs-built_in\">set</span> ").append(name).append("=").append(value).append("\n");
+                powershellBuilder.append("<span class=\"hljs-variable\">$ENV</span> ").append(name).append(" = <span class=\"hljs-string\">\"").append(value).append("\"</span>\n");
+            }
+            str = str.replace(line, "++++\n" + html.replace("{bash}", bashBuilder.toString()).replace("{windows}", windowsBuilder.toString()).replace("{powershell}", powershellBuilder.toString()) + "\n++++\n");
+        }
+        return str;
+    }
+}
--- guides-cli/src/main/java/io/micronaut/guides/cli/GradleMavenTabs.java
+++ guides-cli/src/main/java/io/micronaut/guides/cli/GradleMavenTabs.java
@@ -1,28 +1,25 @@
 package io.micronaut.guides.cli;
 
-import io.micronaut.context.exceptions.ConfigurationException;
 import io.micronaut.core.annotation.NonNull;
 import io.micronaut.core.io.ResourceLoader;
 import io.micronaut.guides.core.Guide;
 import io.micronaut.guides.core.GuidesOption;
 import io.micronaut.guides.core.GuidesTemplatesConfiguration;
+import io.micronaut.guides.core.TemplateLoaderUtils;
 import io.micronaut.guides.core.MacroSubstitution;
 import io.micronaut.guides.core.asciidoc.AsciidocMacro;
 import io.micronaut.guides.core.asciidoc.AsciidocUtils;
-import io.micronaut.starter.options.BuildTool;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import static io.micronaut.guides.core.MacroUtils.findMacroLines;
 
 abstract class GradleMavenTabs implements MacroSubstitution {
     protected String gradleMavenTabsHtml;
+
+    protected GradleMavenTabs(ResourceLoader resourceLoader, GuidesTemplatesConfiguration guidesTemplatesConfiguration) {
+        this.gradleMavenTabsHtml = TemplateLoaderUtils.getClasspathTemplate(resourceLoader, guidesTemplatesConfiguration, "gradleMavenTabs.html");
+    }
 
     @NonNull
     protected abstract String getMacro();
@@ -44,24 +41,5 @@ abstract class GradleMavenTabs implements MacroSubstitution {
                     .replace("{maven}", maven(asciidocMacroOptional.get(), guide, option))));
         }
         return str;
-    }
-
-    protected GradleMavenTabs(ResourceLoader resourceLoader, GuidesTemplatesConfiguration guidesTemplatesConfiguration) {
-        String path = "classpath:" + guidesTemplatesConfiguration.getFolder() + "/gradleMavenTabs.html";
-        Optional<InputStream> inputStreamOptional = resourceLoader.getResourceAsStream(path);
-        if (inputStreamOptional.isEmpty()) {
-            throw new ConfigurationException(path);
-        }
-        try (InputStream inputStream = inputStreamOptional.get()) {
-            this.gradleMavenTabsHtml = readInputStream(inputStream);
-        } catch (Exception e) {
-            throw new ConfigurationException("Error loading resource: " + path, e);
-        }
-    }
-
-    protected static String readInputStream(InputStream inputStream) throws IOException {
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
-            return reader.lines().collect(Collectors.joining(System.lineSeparator()));
-        }
     }
 }
